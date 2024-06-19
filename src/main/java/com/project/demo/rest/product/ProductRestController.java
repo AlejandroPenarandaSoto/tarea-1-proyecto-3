0d362e155beb9abb3a128a01119eae6a8bd1d0b9
AlejandroPenarandaSoto <135558117+AlejandroPenarandaSoto@users.noreply.github.com>
2024-06-19 00:19:37 -0600

    Implementación parcial Producto

--- src/main/java/com/project/demo/rest/product/ProductRestController.java
+++ src/main/java/com/project/demo/rest/product/ProductRestController.java
@@ -1,4 +1,49 @@
 package com.project.demo.rest.product;
 
+import com.project.demo.logic.entity.product.Product;
+import com.project.demo.logic.entity.product.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductRestController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllCategories() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    public Product addCategory(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    public Product updateCategory(@PathVariable Long id, @RequestBody Product product) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setName(product.getName());
+                    return productRepository.save(existingProduct);
+                })
+                .orElseGet(() -> {
+                    product.setProductId(id);
+                    return productRepository.save(product);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    public void deleteCategory(@PathVariable Long id) {
+        productRepository.deleteById(id);
+    }
+
 }
